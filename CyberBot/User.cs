11cc9af9516f442e42045ca9d0555b4989c4e73e
Sanelisiwe <sanelisiwehlophe03@gmail.com>
2026-09-08 10:59:40 +0200

    i have 5 classes which is my program class,services class, the user class , utils class and the audio player class

---
+++ CyberBot/User.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+namespace CyberBot.Models
+{
+    /// <summary>
+    /// Represents the user interacting with CyberBot.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Gets or sets the user's name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Creates a new User with the specified name.
+        /// </summary>
+        /// <param name="name">The user's name.</param>
+        public User(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException(
+                    "User name cannot be empty.",
+                    nameof(name));
+            }
+
+            Name = name.Trim();
+        }
+    }
+}
